97cf358cbbbfa3deef7f3970f8ff67820f32f7a8
Lindsey Choi <lindsey.e.choi@gmail.com>
2022-01-19 14:03:45 -0700

    created js and css and added pseudo-coding to the js file.

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+/**
+From the README:
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+ */
+
+/**
+type and format question
+type and format answers as buttons
+function for timer/countdown
+three wrong answers take time off when clicked
+correct answer click starts next question
+same class and style for all four questions
+use study session app for the initials and score input
+ */
+
+
+
+
+
+//variables
+
+//functions
+
+//logic
+
+//actions
