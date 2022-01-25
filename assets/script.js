3cc1060fa6dd6295952bae9a768552deaf6ffd47
Lindsey Choi <lindsey.e.choi@gmail.com>
2022-01-24 23:02:58 -0700

    set items to local storage then get them and  return them in a list

--- assets/script.js
+++ assets/script.js
@@ -56,187 +56,228 @@ var quizQuestions =
 
   ];
 
-  const space1 = document.getElementById("choiceSpace")
-  const space2 = document.getElementById("choiceSpace1")
-  const space3 = document.getElementById("choiceSpace2")
+const space1 = document.getElementById("choiceSpace")
+const space2 = document.getElementById("choiceSpace1")
+const space3 = document.getElementById("choiceSpace2")
 
 //functions
 
 //function to clear the card containing one question so that the next can populate
+
+function submitScore() {
+  var inputBox = document.getElementById("inputInitials");
+  var initials = inputBox.value.trim();
+  var scoreIndex = localStorage.getItem("scoreList");
+  let scoreList = new Array();
+  var myScore = localStorage.getItem("myScore");
+
+  if (!initials) {
+    alert("Name required. Try Again.");
+    return;
+  }
+  if (scoreIndex) {
+    scoreList = JSON.parse(scoreIndex);
+  }
+
+  scoreList.push({ name: initials, score: myScore });
+  localStorage.setItem("scoreList", JSON.stringify(scoreList));
+  console.log(JSON.parse(localStorage.getItem("scoreList")));
+  inputBox.innerHTML = "";
+  document.getElementById("submitInitials").innerHTML = "";
+  let scorerList = document.getElementById("scorer-list");
+  console.log(scorerList.nodeType);
+  var scorer;
+  var newLi;
+  for (let index = 0; index < scoreList.length; index++) {
+    scorer = scoreList[index];
+    newLi = document.createElement("li");
+    newLi.textContent = scorer.name + scorer.score;
+    scorerList.appendChild(newLi);
+  }
+}
 
 function wrongToLastQues() {
   timeLeft -= 5;
   lastQuestion();
 }
 
-function clearQuestion() {
+  function clearQuestion() {
 
-  space1.innerHTML = "";
-  space2.innerHTML = "";
-  space3.innerHTML = "";
-}
+    space1.innerHTML = "";
+    space2.innerHTML = "";
+    space3.innerHTML = "";
+  }
 
-function wrongToNextQues() {
-  console.log("i got clicked 1");
-  timeLeft -=5;
-  nextQuestion();
-}
+  function wrongToNextQues() {
+    console.log("i got clicked 1");
+    timeLeft -= 5;
+    nextQuestion();
+  }
 
-function wrongToGetScore() {
-  timeLeft -=5;
-  getScore();
-}
+  function wrongToGetScore() {
+    timeLeft -= 5;
+    getScore();
+  }
 
-//function to clear the timer in getScore
-function clearTimerButton() {
-  document.getElementById("timer").innerHTML = "";
-}
+  //function to clear the timer in getScore
+  function clearTimerButton() {
+    document.getElementById("timer").innerHTML = "";
+  }
 
-//call current question in a function so that the card can clear before the next question appears
-function setObjTextById(objId, text) {
+  //call current question in a function so that the card can clear before the next question appears
+  function setObjTextById(objId, text) {
 
-  document.getElementById(objId).innerText = text;
-}
+    document.getElementById(objId).innerText = text;
+  }
 
-//call function with the eventlistener at the bottom when start is clicked, starts the timer/countdown
-function countDown() {
-  var timerInterval = setInterval(function () {
-    timeLeft--;
-    timeEl.textContent = timeLeft;
-
-    if (timeLeft === 0) {
-      timeLeft = 0;
-      clearInterval(timeLeft);
+  //call function with the eventlistener at the bottom when start is clicked, starts the timer/countdown
+  function countDown() {
+    var timerInterval = setInterval(function () {
+      timeLeft--;
       timeEl.textContent = timeLeft;
-      getScore();
-    }
 
-    if (getScore) {
-      clearInterval(timeLeft);
-    }
-  }, 1000);
-}
+      if (timeLeft === 0) {
+        timeLeft = 0;
+        clearInterval(timeLeft);
+        timeEl.textContent = timeLeft;
+        getScore();
+      }
 
-//tell user time is at 0, enter initials and score into local storage
+      if (getScore) {
+        clearInterval(timeLeft);
+      }
+    }, 1000);
+  }
 
-//logic
-function createButton(appendElement, buttonText) {
-  var btn = document.createElement("button");
-  btn.innerHTML = buttonText;
-  appendElement.appendChild(btn);
-  return btn;
-}
+  //tell user time is at 0, enter initials and score into local storage
 
-function startQuiz() {
-  document.getElementById("strButton").innerHTML = "";
+  //logic
+  function createButton(appendElement, buttonText) {
+    var btn = document.createElement("button");
+    btn.innerHTML = buttonText;
+    appendElement.appendChild(btn);
+    return btn;
+  }
 
-  var questionSpaceId = "questionSpace";
-  var questionCounter = 0;
-  var choiceSpaceId = "choiceSpace";
-  var choiceSpace = document.getElementById(choiceSpaceId);
-  var choiceSpaceId1 = "choiceSpace1";
-  var choiceSpace1 = document.getElementById(choiceSpaceId1);
-  var choiceSpaceId2 = "choiceSpace2";
-  var choiceSpace2 = document.getElementById(choiceSpaceId2);
-  console.log(quizQuestions);
+  function startQuiz() {
+    document.getElementById("strButton").innerHTML = "";
 
-  // ask questions
-  setObjTextById(questionSpaceId, quizQuestions[0].question);
+    var questionSpaceId = "questionSpace";
+    var questionCounter = 0;
+    var choiceSpaceId = "choiceSpace";
+    var choiceSpace = document.getElementById(choiceSpaceId);
+    var choiceSpaceId1 = "choiceSpace1";
+    var choiceSpace1 = document.getElementById(choiceSpaceId1);
+    var choiceSpaceId2 = "choiceSpace2";
+    var choiceSpace2 = document.getElementById(choiceSpaceId2);
+    console.log(quizQuestions);
 
-  createButton(choiceSpace, quizQuestions[questionCounter].choices[0]);
-  createButton(choiceSpace1, quizQuestions[questionCounter].choices[1]);
-  createButton(choiceSpace2, quizQuestions[questionCounter].choices[2]);
+    // ask questions
+    setObjTextById(questionSpaceId, quizQuestions[0].question);
 
-  space1.addEventListener("click", wrongToNextQues); 
-  space2.addEventListener("click", wrongToNextQues); 
-  space3.addEventListener("click", nextQuestion);
+    createButton(choiceSpace, quizQuestions[questionCounter].choices[0]);
+    createButton(choiceSpace1, quizQuestions[questionCounter].choices[1]);
+    createButton(choiceSpace2, quizQuestions[questionCounter].choices[2]);
 
-  console.log("My current score is " + timeLeft + " after the 1st question");
+    space1.addEventListener("click", wrongToNextQues);
+    space2.addEventListener("click", wrongToNextQues);
+    space3.addEventListener("click", nextQuestion);
 
-}
+    console.log("My current score is " + timeLeft + " after the 1st question");
 
-function nextQuestion() {
-  console.log("next question is ran");
-  clearQuestion();
+  }
 
-  space1.removeEventListener("click", wrongToNextQues);
-  space2.removeEventListener("click", wrongToNextQues);
-  space3.removeEventListener("click", nextQuestion);
+  function nextQuestion() {
+    console.log("next question is ran");
+    clearQuestion();
 
-  // document.getElementById("choiceSpace").removeEventListener("click", wrongToNextQues);
-  // document.getElementById("choiceSpace1").removeEventListener("click", wrongToNextQues);
-  // document.getElementById("choiceSpace2").removeEventListener("click", nextQuestion);
+    space1.removeEventListener("click", wrongToNextQues);
+    space2.removeEventListener("click", wrongToNextQues);
+    space3.removeEventListener("click", nextQuestion);
 
-  var questionSpaceId = "questionSpace";
-  var questionCounter = 1;
-  var choiceSpaceId = "choiceSpace";
-  var choiceSpace = document.getElementById(choiceSpaceId);
-  var choiceSpaceId1 = "choiceSpace1";
-  var choiceSpace1 = document.getElementById(choiceSpaceId1);
-  var choiceSpaceId2 = "choiceSpace2";
-  var choiceSpace2 = document.getElementById(choiceSpaceId2);
-  console.log(quizQuestions);
+    // document.getElementById("choiceSpace").removeEventListener("click", wrongToNextQues);
+    // document.getElementById("choiceSpace1").removeEventListener("click", wrongToNextQues);
+    // document.getElementById("choiceSpace2").removeEventListener("click", nextQuestion);
 
-  // ask questions
-  setObjTextById(questionSpaceId, quizQuestions[1].question);
+    var questionSpaceId = "questionSpace";
+    var questionCounter = 1;
+    var choiceSpaceId = "choiceSpace";
+    var choiceSpace = document.getElementById(choiceSpaceId);
+    var choiceSpaceId1 = "choiceSpace1";
+    var choiceSpace1 = document.getElementById(choiceSpaceId1);
+    var choiceSpaceId2 = "choiceSpace2";
+    var choiceSpace2 = document.getElementById(choiceSpaceId2);
+    console.log(quizQuestions);
 
-  createButton(choiceSpace, quizQuestions[questionCounter].choices[0]);
-  createButton(choiceSpace1, quizQuestions[questionCounter].choices[1]);
-  createButton(choiceSpace2, quizQuestions[questionCounter].choices[2]);
+    // ask questions
+    setObjTextById(questionSpaceId, quizQuestions[1].question);
 
-  document.getElementById("choiceSpace").addEventListener("click", wrongToLastQues);
-  document.getElementById("choiceSpace1").addEventListener("click", lastQuestion); 
-  document.getElementById("choiceSpace2").addEventListener("click", wrongToLastQues);
+    createButton(choiceSpace, quizQuestions[questionCounter].choices[0]);
+    createButton(choiceSpace1, quizQuestions[questionCounter].choices[1]);
+    createButton(choiceSpace2, quizQuestions[questionCounter].choices[2]);
 
-
-  console.log("My current score is " + timeLeft + " after the 2nd question");
-}
-
-function lastQuestion() {
-  clearQuestion();
-
-  space1.removeEventListener("click", wrongToLastQues);
-  space2.removeEventListener("click", lastQuestion);
-  space3.removeEventListener("click", wrongToLastQues);
-
-  var questionSpaceId = "questionSpace";
-  var questionCounter = 2;
-  var choiceSpaceId = "choiceSpace";
-  var choiceSpace = document.getElementById(choiceSpaceId);
-  var choiceSpaceId1 = "choiceSpace1";
-  var choiceSpace1 = document.getElementById(choiceSpaceId1);
-  var choiceSpaceId2 = "choiceSpace2";
-  var choiceSpace2 = document.getElementById(choiceSpaceId2);
-  console.log(quizQuestions);
-
-  // ask questions
-  setObjTextById(questionSpaceId, quizQuestions[2].question);
-
-  createButton(choiceSpace, quizQuestions[questionCounter].choices[0]);
-  createButton(choiceSpace1, quizQuestions[questionCounter].choices[1]);
-  createButton(choiceSpace2, quizQuestions[questionCounter].choices[2]);
-
-  document.getElementById("choiceSpace").addEventListener("click", wrongToGetScore);
-  document.getElementById("choiceSpace1").addEventListener("click", getScore); 
-  document.getElementById("choiceSpace2").addEventListener("click", wrongToGetScore);
-
-  console.log("My current score is " + timeLeft + " after the 3rd question");
-
-}
-
-function getScore() {
-  clearQuestion();
-  document.getElementById("questionSpace").innerHTML = "";
-  console.log("getScore is working");
-  var myScore = timeLeft;
-  console.log("myScore is " + myScore + "!");
-  document.getElementById("questionSpace").innerHTML = "Your score is: " + myScore + "! Enter your score and initials below.";
-}
+    document.getElementById("choiceSpace").addEventListener("click", wrongToLastQues);
+    document.getElementById("choiceSpace1").addEventListener("click", lastQuestion);
+    document.getElementById("choiceSpace2").addEventListener("click", wrongToLastQues);
 
 
+    console.log("My current score is " + timeLeft + " after the 2nd question");
+  }
 
-//actions
-document.getElementById("strButton").addEventListener("click", countDown);
-document.getElementById("strButton").addEventListener("click", startQuiz);
+  function lastQuestion() {
+    clearQuestion();
+
+    space1.removeEventListener("click", wrongToLastQues);
+    space2.removeEventListener("click", lastQuestion);
+    space3.removeEventListener("click", wrongToLastQues);
+
+    var questionSpaceId = "questionSpace";
+    var questionCounter = 2;
+    var choiceSpaceId = "choiceSpace";
+    var choiceSpace = document.getElementById(choiceSpaceId);
+    var choiceSpaceId1 = "choiceSpace1";
+    var choiceSpace1 = document.getElementById(choiceSpaceId1);
+    var choiceSpaceId2 = "choiceSpace2";
+    var choiceSpace2 = document.getElementById(choiceSpaceId2);
+    console.log(quizQuestions);
+
+    // ask questions
+    setObjTextById(questionSpaceId, quizQuestions[2].question);
+
+    createButton(choiceSpace, quizQuestions[questionCounter].choices[0]);
+    createButton(choiceSpace1, quizQuestions[questionCounter].choices[1]);
+    createButton(choiceSpace2, quizQuestions[questionCounter].choices[2]);
+
+    document.getElementById("choiceSpace").addEventListener("click", wrongToGetScore);
+    document.getElementById("choiceSpace1").addEventListener("click", getScore);
+    document.getElementById("choiceSpace2").addEventListener("click", wrongToGetScore);
+
+    console.log("My current score is " + timeLeft + " after the 3rd question");
+
+  }
+
+  function getScore() {
+    clearQuestion();
+    document.getElementById("questionSpace").innerHTML = "";
+    console.log("getScore is working");
+    var myScore = timeLeft;
+    console.log("myScore is " + myScore + "!");
+    document.getElementById("questionSpace").innerHTML = "Your score is: " + myScore + "! Enter your score and initials below.";
+    localStorage.setItem("myScore", myScore);
+    var inputBox = document.createElement("input");
+    var container = document.getElementById("initialsInput");
+    inputBox.id = "inputInitials"
+    container.appendChild(inputBox);
+
+    createButton(document.getElementById("submitInitials"), "Submit");
+    document.getElementById("submitInitials").addEventListener("click", submitScore);
+
+  }
+
+
+
+  //actions
+  document.getElementById("strButton").addEventListener("click", countDown);
+  document.getElementById("strButton").addEventListener("click", startQuiz);
 
 
